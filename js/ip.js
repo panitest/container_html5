096c800146aac9b797d05de34c5d952ff9836b0e
panitest <andres@genesinteractive.com>
2014-11-13 18:18:23 -0300

    ip
    ip

--- js/ip.js
+++ js/ip.js
@@ -31,7 +31,7 @@ function actionDescarga(url){
 	var h=document.documentElement.clientHeight;
 	$('mapa').style.height=h+'px';
 	$('mapa').style.visibility='visible';
-	$('mapa').innerHTML='<iframe style="margin-top:62px;" src="'+url+'?latitud='+ns.mipos[0]+'&longitud='+ns.mipos[1]+'&'+(+new Date())+'&user_id='+ns.deviceData[0]+'&os_version='+ns.deviceData[1]+'&os='+ns.deviceData[2]+'&mobile_model='+ns.deviceData[3]+'" width="100%" height="'+(h-62)+'" allowtransparency="true" id="appmapa" frameborder="0"></iframe>';
+	$('mapa').innerHTML='<object style="margin-top:62px;" data="'+url+'?latitud='+ns.mipos[0]+'&longitud='+ns.mipos[1]+'&'+(+new Date())+'&user_id='+ns.deviceData[0]+'&os_version='+ns.deviceData[1]+'&os='+ns.deviceData[2]+'&mobile_model='+ns.deviceData[3]+'" width="100%" height="'+(h-62)+'" allowtransparency="true" id="appmapa" frameborder="0"></object>';
 	
 }
 
@@ -251,6 +251,7 @@ function showHideMenu(){
 		$('menulateral').className='menuder';
 	}	
 }
+
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 		$('user_id').value=device.uuid;
